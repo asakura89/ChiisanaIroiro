8f2da8c610de903150e38bffc8deb7c77e518270
asakura89 <flamboyan3no1a@gmail.com>
2019-11-07 19:40:44 +0700

    [Script] TextStatistics

---
+++ Plugin/DefaultPlugin/TextStatisticsView.xaml.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Ayumi.ViewablePlugin;
+
+namespace DefaultPlugin {
+    public partial class TextStatisticsView : UserControl, IViewablePlugin {
+        public String ComponentName => "Text Statistics";
+
+        public String ComponentDesc => "Count lines, words, and characters";
+
+        public UserControl View => this;
+
+        public Object Process(Object processArgs) => throw new InvalidOperationException("Not used.");
+
+        public TextStatisticsView() {
+            InitializeComponent();
+            CommonView.ConfigButtonAccesssor.Visibility = Visibility.Collapsed;
+            CommonView.ProcessButtonAccesssor.Click += ProcessButton_Click;
+        }
+
+        void ProcessButton_Click(Object sender, RoutedEventArgs e) {
+            String[] lines = CommonView.Input
+                .Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            Int32 charsFull = 0, charsStripped = 0;
+            for (Int32 idx = 0; idx < lines.Length; idx++) {
+                Int32 full = lines[idx].Length;
+                Int32 stripped = lines[idx].Trim().Replace(" ", String.Empty).Length;
+                charsFull += full;
+                charsStripped += stripped;
+            }
+
+            Int32 words = 0;
+            for (Int32 idx = 0; idx < lines.Length; idx++) {
+                Int32 inline = lines[idx].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                words += inline;
+            }
+
+            CommonView.Output = new StringBuilder()
+                .AppendLine($"Lines: {lines.Length}")
+                .AppendLine($"Words: {words}")
+                .AppendLine($"Chars full: {charsFull}")
+                .AppendLine($"Chars w/o spaces: {charsStripped}")
+                .ToString();
+        }
+    }
+}
