5af6ff26a04e8956b7ae31da1830718f8c57a402
asakura89 <flamboyan3no1a@gmail.com>
2020-01-24 11:41:28 +0700

    [Script] Still Encode / Decode

--- Plugin/DefaultPlugin/EncodeDecodeView.xaml.cs
+++ Plugin/DefaultPlugin/EncodeDecodeView.xaml.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Security.AntiXss;
 using System.Windows;
@@ -15,18 +18,9 @@ namespace DefaultPlugin {
 
         public Object Process(Object processArgs) => throw new InvalidOperationException("Not used.");
 
-        Boolean HtmlEncode => HtmlEncodeRadio.IsChecked ?? false;
-
-        Boolean HtmlDecode => HtmlDecodeRadio.IsChecked ?? false;
-
-        Boolean UrlEncode => UrlEncodeRadio.IsChecked ?? false;
-
-        Boolean UrlDecode => UrlDecodeRadio.IsChecked ?? false;
-
         public EncodeDecodeView() {
             InitializeComponent();
             CommonView.HideAllButton();
-            HtmlEncodeRadio.IsChecked = true;
         }
 
         void ClipboardButton_Click(Object sender, RoutedEventArgs e) {
@@ -36,16 +30,53 @@ namespace DefaultPlugin {
             }
         }
 
+        static readonly IDictionary<Int32, Func<String[], IEnumerable<String>>> Operations = new Dictionary<Int32, Func<String[], IEnumerable<String>>> {
+            [1] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(input => AntiXssEncoder.HtmlEncode(input, true))),
+            [2] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(input => HttpUtility.HtmlDecode(input))),
+            [3] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(input => AntiXssEncoder.UrlEncode(input).Replace("+", "%20"))),
+            [4] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(input => HttpUtility.UrlDecode(input))),
+            [5] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(input => Convert.ToBase64String(Encoding.UTF8.GetBytes(input)))),
+            [6] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(input => Encoding.UTF8.GetString(Convert.FromBase64String(input)))),
+            [7] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(Base64UrlEncode)),
+            [8] = new Func<String[], IEnumerable<String>>(inputs => inputs.Select(Base64UrlDecode))
+        };
+
         void ProcessButton_Click(Object sender, RoutedEventArgs e) {
-            String input = CommonView.Input;
-            if (HtmlEncode)
-                CommonView.Output = AntiXssEncoder.HtmlEncode(input, true).Replace("+", "%20");
-            else if (HtmlDecode)
-                CommonView.Output = HttpUtility.HtmlDecode(input);
-            else if (UrlEncode)
-                CommonView.Output = AntiXssEncoder.UrlEncode(input);
-            else if (UrlDecode)
-                CommonView.Output = HttpUtility.UrlDecode(input);
+            try {
+                String[] inputs = CommonView.Input.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+                if (Operations.ContainsKey(EncodeDecodeComboBox.SelectedIndex)) {
+                    CommonView.Output = String.Join(
+                        Environment.NewLine,
+                        Operations[EncodeDecodeComboBox.SelectedIndex].Invoke(inputs)
+                    );
+                }
+            }
+            catch (Exception ex) {
+                // NOTE: to catch nonbase64 entered
+                CommonView.Output = "Invalid input.";
+            }
+        }
+
+        static String Base64UrlEncode(String text) =>
+            Convert
+                .ToBase64String(Encoding.UTF8.GetBytes(text))
+                .TrimEnd('=')
+                .Replace("+", "-")
+                .Replace("/", "_");
+
+        static String Base64UrlDecode(String base64Url) {
+            String base64 = base64Url
+                .Replace("-", "+")
+                .Replace("_", "/");
+
+            return Encoding.UTF8.GetString(
+                Convert.FromBase64String(
+                    base64.Length % 4 == 2 ?
+                        base64 + "==" :
+                        base64.Length % 4 == 3 ?
+                            base64 + "=" : base64
+                )
+            );
         }
     }
 }
