28d440f98cc0631b2cdb344c8475529c980ca2c3
asakura89 <flamboyan3no1a@gmail.com>
2021-05-02 22:11:58 +0700

    [New] Dump version to 2.0.2021.7502

--- Core/Plugin.Dialog/Properties/AssemblyVersion.cs
+++ Core/Plugin.Dialog/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.2021.7420")]
-[assembly: AssemblyFileVersion("2.0.2021.7420")]
+[assembly: AssemblyVersion("2.0.2021.7502")]
+[assembly: AssemblyFileVersion("2.0.2021.7502")]
