4b4dfa4075d2047572b0ac2cca16b2962e7c3697
asakura89 <flamboyan3no1a@gmail.com>
2021-05-22 13:56:21 +0700

    [New] Dump version to 2.0.2021.7522

--- Plugin/TextProcessingPlugin/Properties/AssemblyVersion.cs
+++ Plugin/TextProcessingPlugin/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.0.2021.7502")]
-[assembly: AssemblyFileVersion("2.0.2021.7502")]
+[assembly: AssemblyVersion("2.0.2021.7522")]
+[assembly: AssemblyFileVersion("2.0.2021.7522")]
